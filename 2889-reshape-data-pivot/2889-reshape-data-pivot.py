ad7d4ac4f4be4cc8879be15974ceacb994da6a1e
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2024-12-31 22:23:52 +0530

    Time: 407 ms (53.83%), Space: 67 MB (81.25%) - LeetHub

--- 2889-reshape-data-pivot/2889-reshape-data-pivot.py
+++ 2889-reshape-data-pivot/2889-reshape-data-pivot.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
-    return weather.pivot_table(index='month',columns='city',values='temperature')
+    return weather.pivot_table(index='month',columns='city',values='temperature',aggfunc='max')

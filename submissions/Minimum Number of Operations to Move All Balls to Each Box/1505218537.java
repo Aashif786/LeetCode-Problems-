f3dfc485f681fd32724f268ed3edf0ccede9ae21
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-11 21:32:55 +0530

    Add submission 1505218537

---
+++ submissions/Minimum Number of Operations to Move All Balls to Each Box/1505218537.java
@@ -0,0 +1,21 @@
+# Title: Minimum Number of Operations to Move All Balls to Each Box
+# Submission ID: 1505218537
+# Status: Accepted
+# Date: January 11, 2025 at 09:32:49 PM GMT+5:30
+
+class Solution {
+    public int[] minOperations(String boxes) {
+        int  n = boxes.length();
+        int res[]= new int[n];
+        for(int i=0; i<n;i++){
+            int sum=0;
+            if(boxes.charAt(i)=='1')    
+            {
+                for(int j=0; j<n;j++){
+                    res[j]+=Math.abs(j-i);
+                }
+            }
+        }
+        return res;
+    }
+}

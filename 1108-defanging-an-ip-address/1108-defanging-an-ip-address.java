9ab734b412ca2a6288f3cc063cff22dadecc287b
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-11 22:03:14 +0530

    Time: 0 ms (100%), Space: 41.6 MB (58.35%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,5 +1,13 @@
 class Solution {
     public String defangIPaddr(String address) {
-        return address.replaceAll("[^0-9]","[.]");
+        StringBuilder s= new StringBuilder();
+        s.append("");
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.') s.append("[.]");
+            else s.append(address.charAt(i));
+        }
+        
+        return s.toString();
+        // return address.replaceAll("[^0-9]","[.]");
     }
 }

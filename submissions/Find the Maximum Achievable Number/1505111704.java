5eee4ee85e80c86b0dc3d2d836a6343f2eed0a7a
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-11 21:34:25 +0530

    Add submission 1505111704

---
+++ submissions/Find the Maximum Achievable Number/1505111704.java
@@ -0,0 +1,10 @@
+# Title: Find the Maximum Achievable Number
+# Submission ID: 1505111704
+# Status: Accepted
+# Date: January 11, 2025 at 07:30:12 PM GMT+5:30
+
+class Solution {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2*t;
+    }
+}

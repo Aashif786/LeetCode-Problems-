d07c802ded91b8ea13af526b2e7c7e045e3d8c9b
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-12 10:27:39 +0530

    Time: 0 ms (100%), Space: 45.1 MB (11.31%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int nn[]= new int[n*2];
+        for(int i=0 ; i<n ;i++){
+            
+            nn[2*i]=nums[i];
+            nn[2*i+1]=nums[n+i];
+        }
+        return nn;
+    }
+}

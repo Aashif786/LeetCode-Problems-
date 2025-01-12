e9b9f7c9231f66324f21731f4016d462f7909d06
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-12 18:39:51 +0530

    Add submission 1506194144

---
+++ submissions/Find Words Containing Character/1506194144.java
@@ -0,0 +1,19 @@
+# Title: Find Words Containing Character
+# Submission ID: 1506194144
+# Status: Accepted
+# Date: January 12, 2025 at 06:39:44 PM GMT+5:30
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) res.add(i);
+        }
+        return res;
+    }
+}
+
+        

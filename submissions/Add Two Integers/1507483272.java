df437599f8a8cd0c0f4ae858e8b3591783930176
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-13 09:15:46 -0800

    Add submission 1507483272

---
+++ submissions/Add Two Integers/1507483272.java
@@ -0,0 +1,10 @@
+# Title: Add Two Integers
+# Submission ID: 1507483272
+# Status: Accepted
+# Date: January 13, 2025 at 10:45:39 PM GMT+5:30
+
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}

f5f6d5a93c2696cf97866b3b8289a9a5bec59c5a
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-16 11:35:25 -0800

    Add submission 1510822042

---
+++ submissions/Minimum Operations to Make Columns Strictly Increasing/1510822042.java
@@ -0,0 +1,22 @@
+# Title: Minimum Operations to Make Columns Strictly Increasing
+# Submission ID: 1510822042
+# Status: Accepted
+# Date: January 17, 2025 at 01:05:16 AM GMT+5:30
+
+class Solution {
+    public int minimumOperations(int[][] grid) {
+        int x=0;
+        for(int j=0;j<grid[0].length;j++)
+        {
+            for(int i=1;i<grid.length;i++)
+            {
+                if(grid[i][j]<=grid[i-1][j]){
+                    int dif= grid[i-1][j]-grid[i][j]+1;
+                    grid[i][j]+=dif;
+                    x+=dif;
+                }
+            }
+        }
+        return x;
+    }
+}

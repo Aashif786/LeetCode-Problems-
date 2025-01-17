31a69dc76a7149e9e1be780126fb37032ef219e5
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-01-17 06:52:54 -0800

    Add submission 1508225839

---
+++ submissions/Minimum Number of Moves to Seat Everyone/1508225839.java
@@ -0,0 +1,15 @@
+# Title: Minimum Number of Moves to Seat Everyone
+# Submission ID: 1508225839
+# Status: Accepted
+# Date: January 14, 2025 at 04:06:16 PM GMT+5:30
+
+import java.util.Arrays;
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int mv=0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        for(int i=0;i<seats.length;i++) mv+=Math.abs(seats[i]-students[i]);
+        return mv;
+    }
+}

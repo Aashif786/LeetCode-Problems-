464de26472a16d88f6426b91cf03bf290cc506e6
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-03 09:37:11 +0530

    Create file

---
+++ 867-TransposeMatrix/867-TransposeMatrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] mat) {
+    
+        int m = mat.length;
+        int n = mat[0].length;
+        int arr[][] = new int[n][m];
+        for(int i=0;i<m;i++){
+            for(int j = 0;j<n;j++)
+                arr[j][i]=mat[i][j];
+        }
+        return arr;
+    }
+}

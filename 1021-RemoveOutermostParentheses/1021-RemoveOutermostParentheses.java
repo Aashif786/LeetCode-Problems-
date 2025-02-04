0a229dd2b574dcc7f4a2df86d0f389696070d53f
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-04 14:44:20 +0530

    Create file

---
+++ 1021-RemoveOutermostParentheses/1021-RemoveOutermostParentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        String str = "";
+        int count = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                if (count > 0) str+="(";
+                count++;
+            } else {
+                count--;
+                if (count > 0) str+=")";
+            }
+        }
+        return str;
+    }
+}

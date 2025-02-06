ddc9cb06d9c3da1e1b94c68e0dcffa9f396bf494
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-06 14:37:53 +0530

    Create file

---
+++ 23-MergekSortedLists/23-MergekSortedLists.java
@@ -0,0 +1,29 @@
+import java.util.List;
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) return l2;
+        if (l2 == null) return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists.length == 0) return null;
+        return divideAndConquer(lists, 0, lists.length - 1);
+    }
+
+    private ListNode divideAndConquer(ListNode[] lists, int left, int right) {
+        if (left == right) return lists[left];
+
+        int mid = left + (right - left) / 2;
+        ListNode l1 = divideAndConquer(lists, left, mid);
+        ListNode l2 = divideAndConquer(lists, mid + 1, right);
+        return mergeTwoLists(l1, l2);
+    }
+}

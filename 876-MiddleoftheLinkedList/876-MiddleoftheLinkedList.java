53eced486cb02343e89f9eca257296c4ce35c8f8
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-08 00:46:11 +0530

    Create file

---
+++ 876-MiddleoftheLinkedList/876-MiddleoftheLinkedList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        int c = 0;
+        ListNode temp = head;
+        while(temp != null){
+            temp = temp.next;
+            c++;
+        }
+        temp = head;
+        for(int i = 0 ; i<c/2 ; i++){
+            temp = temp.next;
+        }
+        return temp;
+    }
+}

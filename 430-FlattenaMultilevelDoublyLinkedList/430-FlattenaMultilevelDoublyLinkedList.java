100a8dcf6f29d210ba12246da683357d2279ad9c
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-07 17:22:47 +0530

    Create file

---
+++ 430-FlattenaMultilevelDoublyLinkedList/430-FlattenaMultilevelDoublyLinkedList.java
@@ -0,0 +1,31 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
+*/
+
+class Solution {
+    public Node flatten(Node head) {
+        for(Node temp = head; temp!=null ; temp = temp.next)
+        {    
+                if(temp.child!=null)
+                {
+                    Node nex = temp.next;
+                    temp.next = temp.child;
+                    temp.next.prev = temp;
+                    temp.child = null;
+                    Node tail = temp.next;
+                    while(tail.next!=null){
+                        tail = tail.next;
+                    }
+                    tail.next = nex;
+                    if(nex!=null) nex.prev = tail;
+                }
+        }      
+        return head;
+    }
+}

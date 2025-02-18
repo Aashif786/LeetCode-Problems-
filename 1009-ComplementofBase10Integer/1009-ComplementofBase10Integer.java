6de0f9cc793359722c72ddb332ad661e34fc4d2b
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-02-18 09:41:57 +0530

    Create file

---
+++ 1009-ComplementofBase10Integer/1009-ComplementofBase10Integer.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+        StringBuilder sb = new StringBuilder("");
+        for(char ch : Integer.toBinaryString(n).toCharArray()){
+            if(ch == '1') sb.append("0");
+            else sb.append("1");
+        }
+        // System.out.println(sb.toString());
+        return Integer.parseInt(sb.toString(),2);
+    }
+}

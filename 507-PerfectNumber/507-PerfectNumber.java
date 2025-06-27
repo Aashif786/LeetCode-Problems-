0aed0b41a44b01be3d9709fd33526575dabb5ac4
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-06-27 11:48:37 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,6 @@
+// Last updated: 6/27/2025, 11:42:57 AM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        return(num==6 || num==28 || num==496 || num==8128 || num==33550336) ;
+    }
+}

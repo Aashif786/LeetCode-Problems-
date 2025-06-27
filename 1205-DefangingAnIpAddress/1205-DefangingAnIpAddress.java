e1cd41fbc46fac4907da1902da1df75f116af73f
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-06-27 11:47:50 +0530

    Create 1205-DefangingAnIpAddress

---
+++ 1205-DefangingAnIpAddress/1205-DefangingAnIpAddress.java
@@ -0,0 +1,14 @@
+// Last updated: 6/27/2025, 11:42:10 AM
+class Solution {
+    public String defangIPaddr(String address) {
+        StringBuilder s= new StringBuilder();
+        s.append("");
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.') s.append("[.]");
+            else s.append(address.charAt(i));
+        }
+        
+        return s.toString();
+        // return address.replaceAll("[^0-9]","[.]");
+    }
+}

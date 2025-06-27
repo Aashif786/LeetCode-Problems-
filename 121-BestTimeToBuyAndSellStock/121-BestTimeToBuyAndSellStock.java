c394f6f58930881c404d384e190cfb4fd4778f60
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-06-27 11:49:42 +0530

    Create 121-BestTimeToBuyAndSellStock

---
+++ 121-BestTimeToBuyAndSellStock/121-BestTimeToBuyAndSellStock.java
@@ -0,0 +1,13 @@
+// Last updated: 6/27/2025, 11:44:01 AM
+class Solution {
+    public int maxProfit(int[] p) {
+        if (p.length < 2) return 0;
+        int max=0;
+        int min=Integer.MAX_VALUE;
+        for(int pr: p){
+            if (pr<min) min = pr;
+            else if (pr-min > max) max=pr-min;
+        }
+        return max;
+    }
+}

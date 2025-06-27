d63a642aeb1bb7f1c86aeb058ccb41386679193a
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-06-27 11:46:14 +0530

    Create 3379-ScoreOfAString

---
+++ 3379-ScoreOfAString/3379-ScoreOfAString.java
@@ -0,0 +1,12 @@
+// Last updated: 6/27/2025, 11:40:34 AM
+class Solution {
+    public int scoreOfString(String s) {
+        int score=0;
+        for (int i = 0; i < s.length()-1; i++) {
+            if( i+1 < s.length() ){
+                score+= Math.abs((int)s.charAt(i)-(int)s.charAt(i+1));
+            }
+        }
+        return score;
+    }
+}

1ed06ef96ab2d5efbaf9dd8450be8cfe746eb947
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-06-27 16:39:23 +0530

    Create 463-IslandPerimeter

---
+++ 463-IslandPerimeter/version/java/463-IslandPerimeter_20250627_163342.java
@@ -0,0 +1,18 @@
+// Last updated: 6/27/2025, 4:33:42 PM
+public class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int c = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1) {
+                    if ((j > 0 && grid[i][j - 1] == 0) || j == 0) c++;
+                    if ((i > 0 && grid[i - 1][j] == 0) || i == 0) c++;
+                    if ((j < m - 1 && grid[i][j + 1] == 0) || j == m - 1) c++;
+                    if ((i < n - 1 && grid[i + 1][j] == 0) || i == n - 1) c++;
+                }
+            }
+        } return c;
+    }
+}

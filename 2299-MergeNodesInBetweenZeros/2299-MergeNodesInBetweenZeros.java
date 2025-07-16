5c35277d4c9394e8702fe212d1ed4ed348262ded
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-16 11:58:42 +0530

    Create 2299-MergeNodesInBetweenZeros

---
+++ 2299-MergeNodesInBetweenZeros/2299-MergeNodesInBetweenZeros.java
@@ -0,0 +1,30 @@
+// Last updated: 7/16/2025, 11:58:52 AM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode temp = head.next;
+        int sum = 0;
+        ListNode res = new ListNode(0);
+        ListNode x = res;
+        while (temp != null){
+            if(temp.val == 0) {
+                x.next = new ListNode(sum);
+                sum = 0; 
+                x = x.next;
+            }
+            sum += temp.val;
+            temp = temp.next;
+        }
+        return res.next;
+    }
+}

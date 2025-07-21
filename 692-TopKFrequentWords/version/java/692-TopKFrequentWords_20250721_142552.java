df22fb590aad39906879311609c41a36e2079b48
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-21 14:25:39 +0530

    Create 692-TopKFrequentWords

---
+++ 692-TopKFrequentWords/version/java/692-TopKFrequentWords_20250721_142552.java
@@ -0,0 +1,20 @@
+// Last updated: 7/21/2025, 2:25:52 PM
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> l = new ArrayList<>();
+        Map<String, Integer> m = new HashMap<>();
+        for(String i : words) m.put(i, m.getOrDefault(i, 0) + 1);
+        PriorityQueue<String> pq = new PriorityQueue<>((x, y) -> {
+            int c = m.get(x) - m.get(y);
+            if(c == 0) return y.compareTo(x);
+            return c;
+        });
+        for(String w : m.keySet()){
+            pq.offer(w);
+            if(pq.size() > k) pq.poll();
+        }
+        while(!pq.isEmpty()) l.add(pq.poll());
+        Collections.reverse(l);
+        return l;
+    }
+}

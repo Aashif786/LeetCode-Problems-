d466d3441fdeeb8af7e326c375ab9f6455d89cbc
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-28 15:28:30 +0530

    Create 175-CombineTwoTables

---
+++ 175-CombineTwoTables/175-CombineTwoTables.sql
@@ -0,0 +1,5 @@
+-- Last updated: 7/28/2025, 3:28:29 PM
+# Write your MySQL query statement below
+select t1.firstName, t1.lastName , t2.city , t2.state from  
+Person t1  left join Address t2
+on t1.personId=t2.personId;

d53021f68fc55e71f977edc438ab9321d0d0c4f4
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-28 15:26:24 +0530

    Create 1292-ImmediateFoodDeliveryIi

---
+++ 1292-ImmediateFoodDeliveryIi/1292-ImmediateFoodDeliveryIi.sql
@@ -0,0 +1,4 @@
+-- Last updated: 7/28/2025, 3:26:23 PM
+select round(sum(if(order_date = customer_pref_delivery_date,1,0)) /count(*)*100,2)  
+as immediate_percentage from delivery 
+where (customer_id,order_date) in (Select customer_id, min(order_date) from Delivery group by customer_id)

c84bdd4d574726e187a37961f4b68cc94e8f936f
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-28 15:26:27 +0530

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,11 @@
+-- Last updated: 7/28/2025, 3:26:25 PM
+select substr(trans_date,1,7) AS month,
+country,
+count(id) as trans_count,
+sum(CASE WHEN state = 'approved' THEN 1 else 0 END) as approved_count,
+sum(amount) as trans_total_amount,
+sum(CASE WHEN state = 'approved' THEN amount else 0 end)  as approved_total_amount
+
+from transactions
+
+group by month,country ;

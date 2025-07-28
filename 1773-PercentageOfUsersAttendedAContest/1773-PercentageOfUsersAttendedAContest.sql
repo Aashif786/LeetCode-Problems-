c40e8be7a4fa49eea159ece8be49cf2a68bdfe83
Aashif Shadin K N <143259498+Aashif786@users.noreply.github.com>
2025-07-28 15:25:52 +0530

    Create 1773-PercentageOfUsersAttendedAContest

---
+++ 1773-PercentageOfUsersAttendedAContest/1773-PercentageOfUsersAttendedAContest.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/28/2025, 3:25:51 PM
+# Write your MySQL query statement below
+with cte as (
+    select count(*) as ct from users
+)
+select r.contest_id, round(count(r.user_id)*100/(c.ct),2) as percentage 
+from register r, cte c
+group by contest_id
+order by percentage desc , contest_id;
